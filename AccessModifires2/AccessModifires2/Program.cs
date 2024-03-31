898cc6a6ad1785e6d08458dc3f7da3f2511db35b
BaleusBlueberry <ssaasshh29@gmail.com>
2024-03-31 18:10:42 +0300

    private and protected

---
+++ AccessModifires2/AccessModifires2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AccessModifires2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Animal catis = new Cat();
+
+        catis.MakeSound();
+
+        Console.ReadLine();
+    }
+}
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("generic animal sound ");
+
+
+
+    }
+
+    protected void MakeAnotherSound()
+    {
+        Console.WriteLine("another sound");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("mewing sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("mewing sound");
+    }
+}
