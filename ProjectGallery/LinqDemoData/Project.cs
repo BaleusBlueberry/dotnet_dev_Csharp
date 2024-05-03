350589868af66c3991c7ff81d08b416759f31fac
BaleusBlueberry <ssaasshh29@gmail.com>
2024-05-03 12:27:03 +0300

    seperated projects and added LinqDemoData to the main projects

---
+++ ProjectGallery/LinqDemoData/Project.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace LinqDemoData;
+
+public class Project : IProjectMeta
+{
+    public string Name { get; set; } = "Joker-App";
+
+    public BitmapImage Icon
+    {
+        get
+        {
+            string? assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            Uri uri = new Uri($"pack://application:,,,/{assemblyName};component/Resources/test.png");
+            return new BitmapImage(uri);
+        }
+    }
+
+
+    public void Run()
+    {
+        MainWindow window = new MainWindow();
+        window.ShowDialog();
+    }
+}
+
--- ProjectGallery/ProjectGallery/MainWindow.xaml.cs
+++ ProjectGallery/ProjectGallery/MainWindow.xaml.cs
@@ -10,10 +10,14 @@ namespace ProjectGallery;
 public partial class MainWindow : Window
 {
     private IProjectMeta[] projects = new IProjectMeta[] {
-        new PersonManager.Project(),
         new Tic_Tac_Toe.Project(),
         new ClashOfClansHelper.Project(),
         new JokeApp.Project(),
+    };
+
+    private IProjectMeta[] testProjects = new IProjectMeta[]
+    {
+        new PersonManager.Project(),
         new UsersAPI.Project(),
         new UsersCRUDApi.Project(),
     };
@@ -31,29 +35,6 @@ public partial class MainWindow : Window
 
         foreach (var project in projects)
         {
-            //Button button = new Button() {
-            //	Width = 100,
-            //	Height = 100,
-            //	Margin = new Thickness(10)
-            //};
-
-            //StackPanel pnl = new StackPanel();
-
-            //pnl.Orientation = Orientation.Vertical;
-
-            //Image img = new Image();
-            //img.Width = 50;
-            //img.Height = 50;
-
-            //TextBlock tb = new TextBlock();
-            //tb.Text = "Button";
-            //tb.VerticalAlignment = VerticalAlignment.Center;
-
-            //pnl.Children.Add(img);
-            //pnl.Children.Add(tb);
-
-            //button.Content = pnl;
-
 
             ProjectButton button = new ProjectButton(project)
             {
@@ -62,6 +43,19 @@ public partial class MainWindow : Window
                 Height = 160
             };
             ProjectsPanel.Children.Add(button);
+        }
+
+        foreach ( var project in testProjects)
+        {
+
+            ProjectButton button = new ProjectButton(project)
+            {
+                Margin = new Thickness(10),
+                Width = 140,
+                Height = 140
+            };
+
+            ProjectsTestsPanel.Children.Add(button);
         }
     }
 
